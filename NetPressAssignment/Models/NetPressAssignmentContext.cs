e7f4d4a2dca61a9bb161407c17ce1d9033e1a50a
shecam13 <sheryl13c@gmail.com>
2015-04-06 14:48:54 +0200

    connection to db

--- NetPressAssignment/Models/NetPressAssignmentContext.cs
+++ NetPressAssignment/Models/NetPressAssignmentContext.cs
@@ -15,7 +15,7 @@ namespace NetPressAssignment.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
     
-        public NetPressAssignmentContext() : base("name=NetPressAssignmentContext")
+        public NetPressAssignmentContext() : base("name=NetPressEntities")
         {
         }
 
