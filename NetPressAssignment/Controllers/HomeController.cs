bff9295afa9faa474e53b5b658a02936e6b1b54e
cvgalea <c.v.galea@gmail.com>
2015-04-12 18:28:10 +0200

    Changes: DateTime & Home Page

--- NetPressAssignment/Controllers/HomeController.cs
+++ NetPressAssignment/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿//using NetPressAssignment.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using NetPressAssignment.Models;
 
 namespace NetPressAssignment.Controllers
 {
     public class HomeController : Controller
     {
+
+        private NetPressAssignmentContext db = new NetPressAssignmentContext();
+
+        Models.Post p = new Models.Post();
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            var posts = db.Posts.Include(p => p.Category).Include(p => p.User); //now
+            return View(posts.ToList());  //now
         }
 
         public ActionResult About()
